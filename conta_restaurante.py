bb580484d9c8b95fdc4e66f8c0054089a0559710
Lucas Mateus <59662718+lucasmateuslid@users.noreply.github.com>
2025-09-24 21:58:51 -0300

    arquivos de pgcp

---
+++ conta_restaurante.py
@@ -0,0 +1,8 @@
+valorConta = float(input("Digite o valor da conta do cliente: "))
+valorRecebido = float(input("Digite o valor recebido pelo cliente: "))
+valorTroco=valorRecebido-valorConta
+print("Devolver o troco: ", valorTroco)
+
+
+# Informar cedulas a serem pagas
+
