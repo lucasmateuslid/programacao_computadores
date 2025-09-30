63c0098b221a7f747cb4b2f5bc807ac1e2bbc23e
Lucas Mateus <59662718+lucasmateuslid@users.noreply.github.com>
2025-09-30 20:10:03 -0300

    Atualizar o conta_restaurante.py

--- conta_restaurante.py
+++ conta_restaurante.py
@@ -1,8 +1,37 @@
 valorConta = float(input("Digite o valor da conta do cliente: "))
 valorRecebido = float(input("Digite o valor recebido pelo cliente: "))
-valorTroco=valorRecebido-valorConta
-print("Devolver o troco: ", valorTroco)
 
+troco = valorRecebido - valorConta
+print("\nTroco total: R$ {:.2f}".format(troco))
 
-# Informar cedulas a serem pagas
+# Considerando só a parte inteira do troco
+troco_int = int(troco)
 
+n100 = troco_int // 100
+troco_int = troco_int % 100
+
+n50 = troco_int // 50
+troco_int = troco_int % 50
+
+n20 = troco_int // 20
+troco_int = troco_int % 20
+
+n10 = troco_int // 10
+troco_int = troco_int % 10
+
+n5 = troco_int // 5
+troco_int = troco_int % 5
+
+n2 = troco_int // 2
+troco_int = troco_int % 2
+
+n1 = troco_int // 1
+
+print("\nCédulas para devolver:")
+print("Notas de 100:", n100)
+print("Notas de 50:", n50)
+print("Notas de 20:", n20)
+print("Notas de 10:", n10)
+print("Notas de 5:", n5)
+print("Notas de 2:", n2)
+print("Notas de 1:", n1)
