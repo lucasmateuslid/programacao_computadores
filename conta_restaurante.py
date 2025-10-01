900f9f4848add48c62ad2bd11d8cb9b08acf9ce5
lucasmateuslid <lucasmateus.e.trabalhos@gmail.com>
2025-09-30 21:53:32 -0300

    Atualizações das atividades

--- conta_restaurante.py
+++ conta_restaurante.py
@@ -1,8 +1,53 @@
 valorConta = float(input("Digite o valor da conta do cliente: "))
 valorRecebido = float(input("Digite o valor recebido pelo cliente: "))
-valorTroco=valorRecebido-valorConta
-print("Devolver o troco: ", valorTroco)
+# Calcula o troco
+troco = valorRecebido - valorConta
+# Considerando só a parte inteira do troco
+troco_int = int(troco)
 
+n200 = troco_int // 200
+troco_int = troco_int % 200
 
-# Informar cedulas a serem pagas
+n100 = troco_int // 100
+troco_int = troco_int % 100
 
+n50 = troco_int // 50
+troco_int = troco_int % 50
+
+n20 = troco_int // 20
+troco_int = troco_int % 20
+
+n10 = troco_int // 10
+troco_int = troco_int % 10
+
+n5 = troco_int // 5
+troco_int = troco_int % 5
+
+n2 = troco_int // 2
+troco_int = troco_int % 2
+
+n1 = troco_int // 1
+
+if troco >= 1:
+    print("Troco total: R$", troco)
+    print("Cédulas para devolver: ")
+    if n200 >= 1:
+        print("Notas de 200: ", n200)
+    if n100 >= 1:
+        print("Notas de 100: ", n100)
+    if n50 >= 1:
+        print("Notas de 50: ", n50)
+    if n20 >= 1:
+        print("Notas de 20: ", n20)
+    if n10 >= 1:
+        print("Notas de 10: ", n10)
+    if n5 >= 1:
+        print("Notas de 5: ", n5)
+    if n2 >= 1:
+        print("Notas de 2: ", n2 )
+    if n1 >= 1:
+        print("Moedas de 1: ", n1)
+elif troco < 0 :
+    print("A cozinha ta cheia de louça pra lavar")
+else:
+    print("Sem valores a devolver.")
--- equacao2grau.py
+++ equacao2grau.py
@@ -3,17 +3,20 @@ b = int(input("Digite o valor de B: "))
 c = int(input("Digite o valor de C: "))
 
 
-# Calcular o valor de delta
-delta_final = b**2 - 4*a*c
-raizDelta = delta_final**0.5
-calc_doisa = 2*a
-# Calculo de x1 
+if a == 0:
+    print("Não é uma equação de segundo grau")
+else:
+    # Calcular o valor de delta
+    delta_final = b**2 - 4*a*c
+    if delta_final < 0:
+        print("Não existem raizes reais")
+    else:
+        raizDelta = delta_final**0.5
+        calc_doisa = 2*a
+        x1 = (-b+raizDelta)/calc_doisa
+        x2 = (-b-raizDelta)/calc_doisa
+        if delta_final > 0:
+            print("As raizes da equação são: x1 =", x1," x2 = ",x2)
+        else:
+            print("Só existe 1 raiz válida: ", x1)
 
-
-x1 = (-b+raizDelta)/calc_doisa
-x2 = (-b-raizDelta)/calc_doisa
-print("As raizes da equação são: x1 =", x1," x2 = ",x2)
-
-
-print("A raiz de Delta: ", raizDelta)
-print("Valor de delta: ", delta_final)
