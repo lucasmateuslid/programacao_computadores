432bca6da152dfc79c73c980824a4d3bdcc92bd1
lucasmateuslid <lucasmateus.e.trabalhos@gmail.com>
2025-11-10 19:23:06 -0300

    atualizações de lista

--- class_pratices/05 - Strings/exercises_string/criptProgram.py
+++ class_pratices/05 - Strings/exercises_string/criptProgram.py
@@ -1,1 +1,10 @@
 #Criar um programa que códifica uma mensagem substituindo cada letra pela próxima letra no alfabeto selecionado pela letra.
+
+messageToCript = input("Digite a sua mensagem para enviar ao aliados: ")
+cryptKey = input("Qual a senha?: ")
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+def CryptMessage():
+    
